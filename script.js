e3d6a9763ea3f06a4138f64f053a7030009fbcb8
Aditya Gaikwad <128017788+AdityaGaikwad03@users.noreply.github.com>
2025-09-21 20:49:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+// colors
+const lavender = "#E6E6FA";
+const coffee = "#6F4E37";
+
+squares.forEach((square, index) => {
+  square.addEventListener("mouseenter", () => {
+    squares.forEach((sq, i) => {
+      if (i !== index) {
+        sq.style.backgroundColor = coffee;
+      } else {
+        sq.style.backgroundColor = lavender;
+      }
+    });
+  });
+
+  square.addEventListener("mouseleave", () => {
+    // reset all to lavender
+    squares.forEach((sq) => {
+      sq.style.backgroundColor = lavender;
+    });
+  });
+});
